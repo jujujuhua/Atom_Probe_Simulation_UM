a7aecf3e16adb06d4055cccf1736413af7f43ef7
jujujuhua <cobbju@umich.edu>
2015-11-19 11:19:01 -0500

    Corrected

--- S3_AssignIniVoltage.cpp
+++ S3_AssignIniVoltage.cpp
@@ -1,7 +1,8 @@
-#include "void S3_AssignIniVoltage.h"
+#include "S3_AssignIniVoltage.h"
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <vector>
 #include <Eigen/Dense>
 using namespace std;
 using Eigen::MatrixXd;
@@ -23,7 +24,7 @@ void S3_AssignIniVoltage(){
 	infile.open(basename);
 	double in = 0.0;
 	string line;
-	std::vector<string> v;
+	vector<string> v;
 	int i = 0;
 	while(getline(infile,line)){
 		v[i] = line;
@@ -32,35 +33,36 @@ void S3_AssignIniVoltage(){
 	MatrixXd Points(v.size(),7);
 	MatrixXd VacuumPoints(v.size(),7);
 	int j = 0;
+	
+	
 	for(i = 0; i < v.size();i++){
 		stringstream ss;
 		ss << v[i];
 		for(int j = 0; j < 6; j ++){
 			ss >> Points(i,j);
 		}
-		Points(i,6) = (Points(i,3)!=0) ? 1 : 0 
-		}
 		if(Points(i,3) == 0){
+			Points(i,6) = Voltage;
 			VacuumPoints.row(j) = Points.row(j).array();
 		}
 	}
-
-	Matrix VoltageInVacuum(VacuumPoints.rows(),1);
+	j+=1;
+	VacuumPoints.resize(j,VacuumPoints.cols());
+	MatrixXd VoltageInVacuum(j,1);
 	VoltageInVacuum.fill(0);
-	
 	double x = 0, y = 0, z = 0;
 	double d2 = 0.0, dmin = 0.0;
-	for(i = 0; i < VoltageInVacuum.rows()){
+	for(i = 0; i < VoltageInVacuum.rows();i++){
 		x = VacuumPoints(i,0);
 		y = VacuumPoints(i,1);
 		z = VacuumPoints(i,2);
 
 		if (z > z0){
-			d2 = exp(x0-x,2)+exp(y0-y,2)+exp(z0-z,2);
+			d2 = pow(x0-x,2)+pow(y0-y,2)+pow(z0-z,2);
 		}else{
-			d2 = exp(x0-x,2)+exp(y0-y,2)
+			d2 = pow(x0-x,2)+pow(y0-y,2);
 		}
-		dmin = exp(d2,0.5)-Radi;
+		dmin = pow(d2,0.5)-Radi;
 
 		VoltageInVacuum(i,0) = (Distance-dmin)*Voltage/Distance;
 		if(VoltageInVacuum(i,0) < 0){
@@ -68,12 +70,11 @@ void S3_AssignIniVoltage(){
 		}
 
 	}	
-
+	
 	for( i =0;i<VacuumPoints.rows();i++){
 		Points(VacuumPoints(i,5),6) = 1;
 	}
 
-save([basename '_iniVoltageAssigned9.5.mat'],'Points');
-	
-	
+//save([basename '_iniVoltageAssigned9.5.mat'],'Points');
+
 }
